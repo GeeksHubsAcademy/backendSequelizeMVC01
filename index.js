04159a31eac1067173c41f4845d7bc0d0d42b963
Dave86dev <davidochando1986@gmail.com>
2022-06-07 13:33:58 +0200

    Puesta en código del modelo MVC

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const port = 3000;
 
 const db = require('./db/db');
 
+const router = require('./router');
+
 //Middleware
 
 app.use(express.json());
+
+app.use(router);
 
 db.then(()=>{
 
---
+++ router.js
@@ -0,0 +1,19 @@
+
+//Imports
+const router = require('express').Router();
+
+const UsersRouter = require('./views/UsersRouter');
+const RentsRouter = require('./views/RentsRouter');
+const SalesRouter = require('./views/SalesRouter');
+const SpecsRouter = require('./views/SpecsRouter');
+const ComputersRouter = require('./views/ComputersRouter');
+
+//Endpoints roots definition
+router.use('/users', UsersRouter);
+router.use('/rents', RentsRouter);
+router.use('/sales', SalesRouter);
+router.use('/specs', SpecsRouter);
+router.use('/computers', ComputersRouter);
+
+//Export
+module.exports = router;
